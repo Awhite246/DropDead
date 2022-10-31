9bba3e115bc77f42ae63834a4688cad7ee1b85fa
Alistiar White <awhite@bsd220.org>
2022-10-31 09:34:18 -0500

    Initial Commit

---
+++ Yahtzee/YahtzeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YahtzeeApp.swift
+//  Yahtzee
+//
+//  Created by Alistair White on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct YahtzeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
