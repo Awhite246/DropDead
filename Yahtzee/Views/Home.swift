d1a35e88bd52edc67f4037c6934bbf8f56cbd078
Alistiar White <awhite@bsd220.org>
2022-11-15 10:10:29 -0600

    Imported 3D Dice File and Set Up Views

---
+++ Yahtzee/Views/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Yahtzee
+//
+//  Created by Alistair White on 11/15/22.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
