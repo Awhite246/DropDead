5c8d69691d065c737ecc369ea9725a249ac9684a
Alistiar White <awhite@bsd220.org>
2022-11-18 10:43:08 -0600

    Working Dice Roll with animation

--- Yahtzee/Views/DiceSceneView.swift
+++ Yahtzee/Views/DiceSceneView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 import SceneKit
 
+//Video Used For Help: https://www.youtube.com/watch?v=d4ciSOLvIH8&t=236s
+//3D Object Credits: "Dice" (https://skfb.ly/Ftpp) by dez_z is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
 struct DiceSceneView: UIViewRepresentable {
     @Binding var scene: SCNScene?
     
     func makeUIView(context: Context) -> SCNView {
+        //Creating all the properties
         let view = SCNView()
-        view.allowsCameraControl = true
+        view.allowsCameraControl = false
         view.antialiasingMode = .multisampling2X
         view.autoenablesDefaultLighting = true
         view.scene = scene
@@ -22,6 +26,7 @@ struct DiceSceneView: UIViewRepresentable {
         return view
     }
     
+    //Dont realy know why u need this but you do
     func updateUIView(_ uiView: SCNView, context: Context) {
         
     }
--- Yahtzee/Views/Home.swift
+++ Yahtzee/Views/Home.swift
@@ -10,39 +10,46 @@ import SceneKit
 import Model3DView
 
 struct Home: View {
-    @State var scene: SCNScene? = .init(named: "Dice.usdz")
-    @State private var currX : Float = 0.0
-    @State private var x : Float = 0.0
-    @State private var y : Float = 0.0
-    @State private var z : Float = 0.0
-    //View Properties
+    //You have to initialize SCNScene up here instead of in DiceSceneView() for some reason or else it won't work
+    @State private var scene: SCNScene? = .init(named: "Dice.usdz")
+    @State private var rolling = false
+    @State private var x : Float = .pi
+    @State private var y : Float = .pi
     var body: some View {
+        //Cool code
         VStack {
             DiceSceneView(scene: $scene)
                 .frame(height: 200)
                 .padding(.top, 50)
             Spacer()
             Button {
-                withAnimation (.linear(duration: 0.5)){
-                    x += ((45 * .pi) / 360)
-                    y += ((45 * .pi) / 360)
-                    z += ((45 * .pi) / 360)
-                    //y += 0.08
-                    //z -= 0.07
-                    //x = Float.random(in: 0...360)
-                    //y = Float.random(in: 0...360)
-                    //z = Float.random(in: 0...360)
-                    //scene?.rootNode.eulerAngles.x = x
+                //Stops user from spamming button
+                rolling = true
+                //Turns on animations
+                SCNTransaction.animationDuration = 2.0
+                
+                //Changes x y z random amount (.pi = 180 rotation)
+                x += (.pi) * Float.random(in: 2.0...4.0)  * (Bool.random() ? -1.0 : 1.0)
+                y += (.pi) * Float.random(in: 2.0...4.0)  * (Bool.random() ? -1.0 : 1.0)
+                //Sets the x y z rotation values to the random numbers
+                scene?.rootNode.eulerAngles.x = x
+                scene?.rootNode.eulerAngles.y = y
+                
+                //Delays by 0.1
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.1) {
+                    //Rounds division to nearest whole number the re-multiplies to center it on a square
+                    x = (round(x / (.pi / 2))) * (.pi / 2)
+                    y = (round(y / (.pi / 2))) * (.pi / 2)
+                    
+                    //Sets recentered xy object rotation
                     scene?.rootNode.eulerAngles.x = x
                     scene?.rootNode.eulerAngles.y = y
-                    scene?.rootNode.eulerAngles.z = z
-                    
+                    rolling = false
                 }
-                
             } label: {
                 Text("Spin")
             }
-            
+            .disabled(rolling)
         }
     }
     
