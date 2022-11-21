dc220960e90a5146707a9aceb9e8160364afcc73
Alistiar White <awhite@bsd220.org>
2022-11-21 10:09:24 -0600

    Put dice vars in struct

--- Yahtzee/Views/Home.swift
+++ Yahtzee/Views/Home.swift
@@ -7,54 +7,75 @@
 
 import SwiftUI
 import SceneKit
-import Model3DView
 
 struct Home: View {
     //You have to initialize SCNScene up here instead of in DiceSceneView() for some reason or else it won't work
-    @State private var scene: SCNScene? = .init(named: "Dice.usdz")
+    @State private var diceScene1: SCNScene? = .init(named: "Dice.usdz")
     @State private var rolling = false
-    @State private var x : Float = .pi
-    @State private var y : Float = .pi
+    @State private var dice1 = Dice()
     var body: some View {
         //Cool code
         VStack {
-            DiceSceneView(scene: $scene)
+            DiceSceneView(scene: $diceScene1)
                 .frame(height: 200)
                 .padding(.top, 50)
             Spacer()
-            Button {
+            Text("\(diceValue())")
+        }
+        .onTapGesture {
+            if !rolling {
                 //Stops user from spamming button
                 rolling = true
                 //Turns on animations
                 SCNTransaction.animationDuration = 2.0
                 
                 //Changes x y z random amount (.pi = 180 rotation)
-                x += (.pi) * Float.random(in: 2.0...4.0)  * (Bool.random() ? -1.0 : 1.0)
-                y += (.pi) * Float.random(in: 2.0...4.0)  * (Bool.random() ? -1.0 : 1.0)
+                dice1.rollDice()
                 //Sets the x y z rotation values to the random numbers
-                scene?.rootNode.eulerAngles.x = x
-                scene?.rootNode.eulerAngles.y = y
-                
+                diceScene1?.rootNode.eulerAngles = dice1.getVector()
                 //Delays by 0.1
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2.1) {
+                    SCNTransaction.animationDuration = 0.75
+                    
                     //Rounds division to nearest whole number the re-multiplies to center it on a square
-                    x = (round(x / (.pi / 2))) * (.pi / 2)
-                    y = (round(y / (.pi / 2))) * (.pi / 2)
+                    dice1.centerDice()
                     
                     //Sets recentered xy object rotation
-                    scene?.rootNode.eulerAngles.x = x
-                    scene?.rootNode.eulerAngles.y = y
+                    diceScene1?.rootNode.eulerAngles = dice1.getVector()
+                    
                     rolling = false
                 }
-            } label: {
-                Text("Spin")
             }
-            .disabled(rolling)
         }
     }
-    
+    func diceValue() -> Int {
+        if rolling {
+            return 0
+        }
+        
+        return -1
+    }
 }
-
+struct Dice: Codable {
+    var xRot : Float = .pi
+    var yRot : Float = .pi
+    var zRot : Float = .pi
+    mutating func rollDice() {
+        //Changes x y z random amount (.pi = 180 rotation)
+        xRot += (.pi) * Float.random(in: 2.5...5.0)  * (Bool.random() ? -1.0 : 1.0)
+        yRot += (.pi) * Float.random(in: 2.5...5.0)  * (Bool.random() ? -1.0 : 1.0)
+        zRot += (.pi) * Float.random(in: 1.0...2.0)  * (Bool.random() ? -1.0 : 1.0)
+    }
+    mutating func centerDice() {
+        //Rounds division to nearest whole number the re-multiplies to center it on a square
+        xRot = (round(xRot / (.pi / 2))) * (.pi / 2)
+        yRot = (round(yRot / (.pi / 2))) * (.pi / 2)
+        zRot = (round(zRot / (.pi))) * (.pi)
+    }
+    func getVector() -> SCNVector3 {
+        return SCNVector3(x: xRot, y: yRot, z: zRot)
+    }
+}
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
