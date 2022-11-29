04c53cbd2a851c567416815fd422f1b4cd89cc62
Alistiar White <awhite@bsd220.org>
2022-11-29 11:09:22 -0600

    Switch dice to observed object, implemented activating / deactivation dice, added point scoring

--- DropDead/Views/Home.swift
+++ DropDead/Views/Home.swift
@@ -11,46 +11,55 @@ import SceneKit
 struct Home: View {
     //Dices
     @State private var diceScene1: SCNScene? = .init(named: "Dice.usdz") //You have to initialize SCNScene up here instead of in DiceSceneView() for some reason or else it won't work
-    @State private var dice1 = Dice()
+    @StateObject private var dice1 = Dice()
     @State private var diceScene2: SCNScene? = .init(named: "Dice.usdz")
-    @State private var dice2 = Dice()
+    @StateObject private var dice2 = Dice()
     @State private var diceScene3: SCNScene? = .init(named: "Dice.usdz")
-    @State private var dice3 = Dice()
+    @StateObject private var dice3 = Dice()
     @State private var diceScene4: SCNScene? = .init(named: "Dice.usdz")
-    @State private var dice4 = Dice()
+    @StateObject private var dice4 = Dice()
     @State private var diceScene5: SCNScene? = .init(named: "Dice.usdz")
-    @State private var dice5 = Dice()
-    @State private var diceScene6: SCNScene? = .init(named: "Dice.usdz")
-    @State private var dice6 = Dice()
+    @StateObject private var dice5 = Dice()
     
     //Stops button presses if dice is currently rolling
     @State private var rolling = false
     //Keeps track of number of times dice has appeared
     @State private var count = [0,0,0,0,0,0]
+    @State private var point = 0
     var body: some View {
+        let diceSize : CGFloat = 100
+        let deactivateColor : Color = .red
         //Cool code
         VStack {
             HStack {
-                DiceSceneView(scene: $diceScene1)
-                    .frame(width: 50, height: 50)
-                    .padding(.top, 50)
-                DiceSceneView(scene: $diceScene2)
-                    .frame(width: 50, height: 50)
-                    .padding(.top, 50)
-                DiceSceneView(scene: $diceScene3)
-                    .frame(width: 50, height: 50)
-                    .padding(.top, 50)
                 DiceSceneView(scene: $diceScene4)
-                    .frame(width: 50, height: 50)
-                    .padding(.top, 50)
+                    .frame(width: diceSize, height: diceSize)
+                    .colorMultiply(dice4.isActivated() ? .white : deactivateColor)
                 DiceSceneView(scene: $diceScene5)
-                    .frame(width: 50, height: 50)
-                    .padding(.top, 50)
-                DiceSceneView(scene: $diceScene6)
-                    .frame(width: 50, height: 50)
-                    .padding(.top, 50)
+                    .frame(width: diceSize, height: diceSize)
+                    .colorMultiply(dice5.isActivated() ? .white : deactivateColor)
             }
+            .padding(.top, 50)
+            HStack {
+                DiceSceneView(scene: $diceScene1)
+                    .frame(width: diceSize, height: diceSize)
+                    .colorMultiply(dice1.isActivated() ? .white : deactivateColor)
+                DiceSceneView(scene: $diceScene2)
+                    .frame(width: diceSize, height: diceSize)
+                    .colorMultiply(dice2.isActivated() ? .white : deactivateColor)
+                DiceSceneView(scene: $diceScene3)
+                    .frame(width: diceSize, height: diceSize)
+                    .colorMultiply(dice3.isActivated() ? .white : deactivateColor)
+            }
+            .padding()
+            Text("Points : \(point)")
+                .font(.title).bold()
             Spacer()
+            Button("Reset Dice") {
+                resetDice()
+                updateDice(animationTime: 0.0)
+            }
+            .disabled(rolling)
             //Displays all count values
             ForEach(0..<6, id: \.self) { num in
                 Text("Count \(num + 1): \(count[num])")
@@ -60,31 +69,20 @@ struct Home: View {
                 if !rolling {
                     //Stops user from spamming button
                     rolling = true
-                    //Turns on animations
-                    SCNTransaction.animationDuration = 2.0
                     
                     //Changes x y z random amount (.pi = 180 rotation)
-                    dice1.rollDice()
-                    dice2.rollDice()
-                    dice3.rollDice()
-                    dice4.rollDice()
-                    dice5.rollDice()
-                    dice6.rollDice()
+                    rollDice()
                     //Sets the x y z rotation values to the random numbers
-                    updateDice()
+                    updateDice(animationTime: 2.0)
                     //Delays by 2.0 seconds, so centering of dice appears as seperate animation
                     DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                        SCNTransaction.animationDuration = 0.5
                         
                         //Rounds division to nearest whole number the re-multiplies to center it on a square
-                        dice1.centerDice()
-                        dice2.centerDice()
-                        dice3.centerDice()
-                        dice4.centerDice()
-                        dice5.centerDice()
-                        dice6.centerDice()
+                        centerDice()
                         //Sets recentered xy object rotation
-                            updateDice()
+                        updateDice(animationTime: 0.5)
+                        updatePoints()
+                        checkActivation()
                         rolling = false
                         //For debugging (I think random value arent random enough)
                         count[dice1.getValue() - 1] += 1
@@ -92,48 +90,104 @@ struct Home: View {
                         count[dice3.getValue() - 1] += 1
                         count[dice4.getValue() - 1] += 1
                         count[dice5.getValue() - 1] += 1
-                        count[dice6.getValue() - 1] += 1
                     }
                 }
             }
             .disabled(rolling)
         }
     }
-    func updateDice() {
+    func resetDice() {
+        dice1.resetDice()
+        dice2.resetDice()
+        dice3.resetDice()
+        dice4.resetDice()
+        dice5.resetDice()
+        point = 0
+    }
+    func rollDice() {
+        dice1.rollDice()
+        dice2.rollDice()
+        dice3.rollDice()
+        dice4.rollDice()
+        dice5.rollDice()
+    }
+    func centerDice() {
+        dice1.centerDice()
+        dice2.centerDice()
+        dice3.centerDice()
+        dice4.centerDice()
+        dice5.centerDice()
+    }
+    func updatePoints() {
+        if dice1.isActivated() { point += dice1.getValue() }
+        if dice2.isActivated() { point += dice2.getValue() }
+        if dice3.isActivated() { point += dice3.getValue() }
+        if dice4.isActivated() { point += dice4.getValue() }
+        if dice5.isActivated() { point += dice5.getValue() }
+    }
+    //updates dice position
+    func updateDice(animationTime : Double) {
+        SCNTransaction.animationDuration = animationTime
         diceScene1?.rootNode.eulerAngles = dice1.getVector()
         diceScene2?.rootNode.eulerAngles = dice2.getVector()
         diceScene3?.rootNode.eulerAngles = dice3.getVector()
         diceScene4?.rootNode.eulerAngles = dice4.getVector()
         diceScene5?.rootNode.eulerAngles = dice5.getVector()
-        diceScene6?.rootNode.eulerAngles = dice6.getVector()
+    }
+    //checks if rolled 2 or 5
+    func checkActivation() {
+        dice1.dropDead()
+        dice2.dropDead()
+        dice3.dropDead()
+        dice4.dropDead()
+        dice5.dropDead()
     }
 }
-struct Dice: Codable {
+class Dice : ObservableObject {
     //xyz value of dice
     private var xRot : Float = 0
     private var yRot : Float = 0
     private var zRot : Float = 0
     //dice activation value
     private var activated : Bool = true
-    mutating func rollDice(reroll : Bool = false) {
+    func resetDice() {
+        xRot = 0
+        yRot = 0
+        zRot = 0
+        activated = true
+    }
+    func rollDice(reroll : Bool = false) {
+        if !activated { return }
+        let xRandom = (.pi / 2) * (4 + Float.random(in: 0...4))  * (Bool.random() ? -1.0 : 1.0)
+        let yRandom = (.pi / 2) * (4 + Float.random(in: 0...4))  * (Bool.random() ? -1.0 : 1.0)
         //Changes x y z random amount (.pi = 180 rotation)
-        xRot += (.pi / 2) * (4 + Float.random(in: 0...4))  * (Bool.random() ? -1.0 : 1.0)
-        yRot += (.pi / 2) * (4 + Float.random(in: 0...4))  * (Bool.random() ? -1.0 : 1.0)
+        xRot += xRandom
+        yRot += yRandom
+        //fixes randomness
         if ((Bool.random() || reroll) && (getValue() == 2 || getValue() == 5)){
+            xRot -= xRandom
+            yRot -= yRandom
             rollDice(reroll: true)
         }
         zRot += (.pi) * Float.random(in: 1.0...3.0)  * (Bool.random() ? -1.0 : 1.0)
     }
-    mutating func centerDice() {
+    func centerDice() {
         //Rounds division to nearest whole number the re-multiplies to center it on a square
         xRot = (round(xRot / (.pi / 2))) * (.pi / 2)
         yRot = (round(yRot / (.pi / 2))) * (.pi / 2)
         zRot = (round(zRot / (.pi))) * (.pi)
     }
-    mutating func setActivation(activate : Bool) {
+    func dropDead(){
+        if getValue() == 2 || getValue() == 5 {
+            setActivation(false)
+        } else {
+            setActivation(true)
+        }
+    }
+    func setActivation(_ activate : Bool) {
         activated = activate
     }
-    mutating func toggle() {
+    func toggle() {
         activated.toggle()
     }
     func getVector() -> SCNVector3 {
